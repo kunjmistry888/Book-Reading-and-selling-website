0955580b412cb9479a8774a84b8384db8485348e
kunjmistry888 <127011071+kunjmistry888@users.noreply.github.com>
2024-04-15 20:14:40 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const toggler = document.querySelector(".toggler");
+const starterPrice = document.getElementById("starter-price");
+const SuperPrice = document.getElementById("Super-price");
+const PremiumPrice = document.getElementById("Premium-price");
